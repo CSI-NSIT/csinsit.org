e41cccb9b22cac87f980943052cdede59bcf18c3
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-08-30 20:58:01 +0530

    href tag values fix

--- index.php
+++ index.php
@@ -30,12 +30,13 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="fa fa-bars color-white"></span>
 					</button>
-					<h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;">			CSI NSIT
-					</a></h1>
+					<h1>
+						<a class="navbar-brand" href="/" data-0="line-height:90px;" data-300="line-height:50px;">CSI NSIT</a>
+					</h1>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav" data-0="margin-top:20px;" data-300="margin-top:5px;">
-						<li class="active"><a href="index.php">Home</a></li>
+						<li class="active"><a href="/">Home</a></li>
 						<li><a href="http://csinsit.org/techbytes/">Blog</a></li>
 						<li><a href="meet-the-team.php">Team</a></li>
 						<li><a href="#section-contact">Contact</a></li>
