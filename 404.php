ac8c6ca49e75a397484edced56630f0070b52442
kamsuri <kamsuri5@gmail.com>
2016-10-31 11:40:03 +0530

    Updated 404 error page

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@
 	<div class="container" style="padding-top:70px;">
 		<div class="row pages-header">What the...<br>404</div>
 		<div class="row pages-header-descp">
-			<center>This page does not exist on our server.<br><a href="/">Click Here</a> to go back to Home Page</center>
+			<center>This page does not exist on our server.<br><a href="http://www.csinsit.org">Click Here</a> to go back to Home Page</center>
 		</div>
 	</div>
 </body>
